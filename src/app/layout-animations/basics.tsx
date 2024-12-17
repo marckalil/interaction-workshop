cda3a417f141c06352cc4d214743c68685cdbf0c
Marc Kalil <marc@marckalil.com>
2024-12-17 18:23:24 +0100

    add cta to layout basics

--- src/app/layout-animations/basics.tsx
+++ src/app/layout-animations/basics.tsx
@@ -1,24 +1,44 @@
-import { color, radius } from '@/src/constants';
-import { StyleSheet, View } from 'react-native';
+import { color, radius, spacing } from '@/src/constants';
+import { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function Index() {
+  const [showsSquare, setShowsSquare] = useState(false);
+  const toggleSquare = () => {
+    setShowsSquare((prev) => !prev);
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.square} />
+      <View style={styles.squareContainer}>
+        {showsSquare && <View style={styles.square} />}
+      </View>
+      <TouchableOpacity onPress={toggleSquare}>
+        <Text style={styles.button}>{showsSquare ? 'Hide' : 'Show'}</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
+const SQUARE_SIZE = 100;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
     justifyContent: 'center',
-    alignItems: 'center'
+    gap: spacing.M
+  },
+  squareContainer: {
+    height: SQUARE_SIZE
   },
   square: {
-    width: 100,
-    height: 100,
+    width: SQUARE_SIZE,
+    height: SQUARE_SIZE,
     backgroundColor: color.blue,
     borderRadius: radius.M
+  },
+  button: {
+    fontSize: 20,
+    fontWeight: 'bold'
   }
 });
